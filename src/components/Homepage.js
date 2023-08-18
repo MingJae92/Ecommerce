3bd6ce167009b03ced3335beeebeafe9fb9c08b2
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-08-18 02:54:42 +0100

    Homepage header realigned

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from '@mui/material';
 
 function Homepage() {
   return (
-    <div>
-      <h1>Mings SUPER STORE!!!</h1>
-    </div>
-  )
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: '10vh' }}
+    >
+      <Grid item>
+        <h1>Mings SUPER STORE!!!</h1>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default Homepage
+export default Homepage;
