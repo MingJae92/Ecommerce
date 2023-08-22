4a93f9c893f729497dac28418b24a7e6870a4c78
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-08-22 03:08:27 +0100

    BE env file added

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,16 +1,37 @@
 import express from "express"
 import dotenv from "dotenv"
-// dotenv.config({ path: '../../config/.env' })
+import mongoose from "mongoose";
+dotenv.config({ path: './config/.env' })
+
+// import mongoose from "mongoose";
 
 const route = express.Router();
 const app = express();
-const port = process.env.PORT || 7000
+const port = process.env.PORT 
+
+async function connectToDataBase() {
+    try {
+        await mongoose.connect(process.env.DBCONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('DB Connected!')
+        
+    }catch(error){
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+connectToDataBase();
+
 
 app.use("/v1", route);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port} here we go!`);
 })
+
+
 
 
 
